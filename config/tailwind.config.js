3f479d7b4a5e004d914503d1435c8306091811ff
Jacob Comer <jacob@jacobcomer.com>
2025-06-22 15:31:24 -0400

    Revert to a more baseline Tailwind configuration

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,28 +2,13 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
+    './app/views/**/*.{html.erb,erb}',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,html}'
   ],
   theme: {
-    screens: {
-      md: '768px',
-      lg: '1000px',
-      xl: '1200px'
-    },
-    fontSize: {
-      sm: '1.1rem',
-      md: '1.23rem',
-      xl: '1.35rem',
-      '2xl': '1.5rem',
-      '3xl': '1.8rem',
-      '4xl': '2.3rem',
-    },
     extend: {
       fontFamily: {
-        sans: ['Inter', 'Open Sans', 'sans-serif'],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
     },
   },
@@ -32,5 +17,5 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-  ]
+  ],
 }
