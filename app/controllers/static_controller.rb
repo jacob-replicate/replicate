aa9dd68e4b7a01a555e6586bf7832a625a359f08
Jacob Comer <jacob@jacobcomer.com>
2025-06-29 19:28:31 -0400

    Generic commit message

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,12 @@
 class StaticController < ApplicationController
   def index
   end
+
+  def terms
+    redirect_to "https://docs.google.com/document/d/1C0zn0671Wg4czBThwsL6bT7Db3btpAsfJepTAMclnuU", allow_other_host: true
+  end
+
+  def privacy
+    redirect_to "https://docs.google.com/document/d/1SZEi3VcuNtLCLhg44WaSDuNTfndmT9BqdF5-djxKEeM", allow_other_host: true
+  end
 end
