94f93416e416926fe47f0ae723cd5d4d7ced938f
Jacob Comer <jacob@jacobcomer.com>
2025-07-13 00:33:35 -0400

    Generic commit message

--- app/services/prompts/coaching_reply.rb
+++ app/services/prompts/coaching_reply.rb
@@ -1,10 +1,4 @@
 module Prompts
-  class CoachingIntro < Prompts::Base
-    def call
-      intro_paragraph = fetch_valid_response
-      return "" if intro_paragraph.nil?
-
-      intro_paragraph + " #{questions.sample}"
-    end
+  class CoachingReply < Prompts::Base
   end
 end
