9cb71d9f7bc908c87dd59f84281f36fdab321b5b
Jacob Comer <jacob@jacobcomer.com>
2025-08-09 11:31:31 -0400

    Generic commit message

--- app/workers/send_cold_email_worker.rb
+++ app/workers/send_cold_email_worker.rb
@@ -5,17 +5,17 @@ class SendColdEmailWorker
   def perform(contact_id, subject, body_html, inbox)
     inbox   = inbox.transform_keys(&:to_sym)
     contact = Contact.find(contact_id)
-    idempotency_key = Sidekiq.redis { |r| r.set("idem:cold_email:#{contact.id}", 1, nx: true, ex: 172_800) }
-    return unless idempotency_key
+    return if contact.contacted?
 
-    from_email     = inbox[:email]
-    from_name      = inbox[:from_name]
-    json_key_path  = inbox[:json_key_path].to_s
-    raise ArgumentError, "missing json_key_path" if json_key_path.empty?
+    from_email    = inbox[:email]
+    from_name     = inbox[:from_name]
+    json_key_path = inbox[:json_key_path]
 
-    json_io = StringIO.new(File.read(json_key_path))
     client = Google::Apis::GmailV1::GmailService.new
-    authorizer = Google::Auth::ServiceAccountCredentials.make_creds(json_key_io: json_io, scope: scopes)
+    authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+      json_key_io: StringIO.new(File.read(json_key_path)),
+      scope: scopes
+    )
     authorizer.update!(sub: from_email)
     client.authorization = authorizer.fetch_access_token!["access_token"]
 
@@ -33,6 +33,7 @@ class SendColdEmailWorker
     ].join("\r\n")
 
     client.send_user_message("me", Google::Apis::GmailV1::Message.new(raw: rfc822))
+    contact.update_columns(contacted: true, contacted_at: Time.current)
   end
 
   def scopes
