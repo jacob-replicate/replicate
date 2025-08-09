801aec798f793e64a1d9792c58a5d30d5cb48e4f
Jacob Comer <jacob@jacobcomer.com>
2025-08-09 11:30:23 -0400

    Generic commit message

--- app/workers/send_cold_email_worker.rb
+++ app/workers/send_cold_email_worker.rb
@@ -14,34 +14,33 @@ class SendColdEmailWorker
     raise ArgumentError, "missing json_key_path" if json_key_path.empty?
 
     json_io = StringIO.new(File.read(json_key_path))
-
     client = Google::Apis::GmailV1::GmailService.new
-    scopes = [
-      "https://mail.google.com",
-      "https://www.googleapis.com/auth/gmail.compose",
-      "https://www.googleapis.com/auth/gmail.modify",
-      "https://www.googleapis.com/auth/gmail.readonly"
-    ]
     authorizer = Google::Auth::ServiceAccountCredentials.make_creds(json_key_io: json_io, scope: scopes)
     authorizer.update!(sub: from_email)
     client.authorization = authorizer.fetch_access_token!["access_token"]
 
-    list_unsub_url  = "https://replicate.info/unsub/#{contact.uuid}"
-    list_unsub_mail = "mailto:#{from_email}?subject=unsubscribe"
-    from_header = from_name.to_s.strip.empty? ? from_email : "#{from_name} <#{from_email}>"
-
     rfc822 = [
       "To: #{contact.email}",
-      "From: #{from_header}",
+      "From: #{from_name} <#{from_email}>",
+      "Reply-To: #{from_email}",
       "Subject: #{subject}",
       "MIME-Version: 1.0",
       "Content-Type: text/html; charset=UTF-8",
-      "List-Unsubscribe: <#{list_unsub_url}>, <#{list_unsub_mail}>",
+      "List-Unsubscribe: <https://replicate.info/unsub/#{contact.uuid}>, <mailto:#{from_email}?subject=unsubscribe>",
       "List-Unsubscribe-Post: List-Unsubscribe=One-Click",
       "",
       body_html
     ].join("\r\n")
 
     client.send_user_message("me", Google::Apis::GmailV1::Message.new(raw: rfc822))
+  end
+
+  def scopes
+    [
+      "https://mail.google.com",
+      "https://www.googleapis.com/auth/gmail.compose",
+      "https://www.googleapis.com/auth/gmail.modify",
+      "https://www.googleapis.com/auth/gmail.readonly"
+    ]
   end
 end
