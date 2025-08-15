f94d94f7c8db72f39c5f2c7f174e35048566dbe4
Jacob Comer <jacob@jacobcomer.com>
2025-08-14 22:09:38 -0400

    Generic commit message

---
+++ app/workers/cron_test_worker.rb
@@ -0,0 +1,9 @@
+class CronTestWorker
+  include Sidekiq::Worker
+
+  def perform(path = "/home/jacob/cron_test.txt")
+    File.open(path, "a") do |file|
+      file.puts Time.now
+    end
+  end
+end
--- config/schedule.rb
+++ config/schedule.rb
@@ -4,13 +4,17 @@ every :monday, at: "6:00am" do
   # runner "ColdEmailGenerator.new(min_score: 90).call"
 end
 
+every 1.minutes do
+  runner "CronTestWorker.perform_async"
+end
+
 every 30.minutes do
   command <<~CMD
-    cd /home/jacob/replicate && \
-    git pull && \
-    bundle install && \
-    whenever --update-crontab && \
-    pkill -f sidekiq || true && \
-    bundle exec sidekiq -c 15 -d -L log/sidekiq.log -P tmp/pids/sidekiq.pid -e production
+    cd /home/jacob/replicate &&
+    git pull &&
+    sudo -n bundle install &&
+    sudo bundle exec whenever --update-crontab &&
+    sudo pkill -f sidekiq || true &&
+    sudo bundle exec sidekiq -c 15 -d -L log/sidekiq.log -P tmp/pids/sidekiq.pid -e production
   CMD
 end
