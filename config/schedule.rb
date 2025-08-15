5860b331102509ef03d0709a0de41515a69a1cc7
Jacob Comer <jacob@jacobcomer.com>
2025-08-15 18:09:59 -0400

    Generic commit message

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,8 @@
 set :output, "/home/jacob/cron_log.log"
+set :environment, "production"
+set :job_template, "bash -l -c ':job'"            # login shell => loads your env
+env :PATH, "/usr/local/bin:/usr/bin:/bin"
+
 job_type :sh, 'bash -lc ":task"'
 
 every :monday, at: "6:00am" do
@@ -11,12 +15,11 @@ end
 
 every 1.minutes do
   sh <<~'CMD'
-    source /home/jacob/.bashrc
-    cd /home/jacob/code/replicate
-    git pull
-    bundle install
-    bundle exec whenever --update-crontab
-    pkill -f sidekiq
+    cd /home/jacob/code/replicate && \
+    git pull && \
+    bundle install && \
+    bundle exec whenever --update-crontab && \
+    pkill -f sidekiq && \
     nohup bundle exec sidekiq -c 15 -e production -L log/sidekiq.log --pidfile tmp/pids/sidekiq.pid >/dev/null 2>&1 &
   CMD
 end
