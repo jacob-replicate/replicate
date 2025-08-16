cbd3586edaf84dd5ebc81be82577934d8fe4593d
Jacob Comer <jacob@jacobcomer.com>
2025-08-15 22:43:25 -0400

    Generic commit message

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,15 +2,19 @@ set :output, "/home/jacob/cron_log.log"
 set :environment, 'production'
 env :PATH, '/home/jacob/.rbenv/shims:/home/jacob/.rbenv/bin:/usr/local/bin:/usr/bin:/bin'
 job_type :sh, "/home/jacob/bin/envwrap bash -lc ':task'"
+job_type :restart_sidekiq, "PATH=/usr/bin:/bin:/usr/sbin:/sbin /bin/systemctl restart replicate-sidekiq.service"
 
 every 1.minutes do
   sh <<~CMD
     cd /home/jacob/code/replicate && \
     git pull && \
     (bundle check || bundle install) && \
-    bundle exec whenever --update-crontab && \
-    systemctl --user restart replicate-sidekiq
+    bundle exec whenever --update-crontab
   CMD
+end
+
+every 1.minutes do
+  restart_sidekiq
 end
 
 every 1.minutes do
