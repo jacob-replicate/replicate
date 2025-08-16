0bc68ee5531d6a77c91453f2e6ebba895374d675
Jacob Comer <jacob@jacobcomer.com>
2025-08-15 21:56:14 -0400

    Generic commit message

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,19 +1,19 @@
 set :output, "/home/jacob/cron_log.log"
-job_type :sh, 'bash -lc ":task"'
+set :environment, 'production'
+env :PATH, '/home/jacob/.rbenv/shims:/home/jacob/.rbenv/bin:/usr/local/bin:/usr/bin:/bin'
+job_type :sh, "/home/jacob/bin/envwrap bash -lc ':task'"
 
-every :monday, at: "6:00am" do
-  # runner "ColdEmailGenerator.new(min_score: 90).call"
+every 1.minutes do
+  sh <<~CMD
+    cd /home/jacob/code/replicate &&
+    git pull &&
+    bundle install &&
+    bundle exec whenever --update-crontab &&
+    pkill -f sidekiq || true
+    nohup bundle exec sidekiq -e production -c 15 -L log/sidekiq.log --pidfile tmp/pids/sidekiq.pid >/dev/null 2>&1 &
+  CMD
 end
 
 every 1.minutes do
-  sh <<~'CMD'
-    cd /home/jacob/code/replicate && \
-    git pull && \
-    bundle install && \
-    bundle exec whenever --update-crontab && \
-    pkill -f sidekiq && \
-    nohup bundle exec sidekiq -c 15 -L log/sidekiq.log --pidfile tmp/pids/sidekiq.pid >/dev/null 2>&1 &
-  CMD
-
-  runner "CronTestWorker.perform_async"
+  sh "cd /home/jacob/code/replicate && bin/rails runner -e production 'CronTestWorker.perform_async' >> /home/jacob/cron_log.log 2>&1"
 end
