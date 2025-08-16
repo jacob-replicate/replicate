5e9f73c1f968acd968175cbebc53a08715be8440
Jacob Comer <jacob@jacobcomer.com>
2025-08-15 22:18:25 -0400

    Generic commit message

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,17 +5,15 @@ job_type :sh, "/home/jacob/bin/envwrap bash -lc ':task'"
 
 every 1.minutes do
   sh <<~CMD
-    cd /home/jacob/code/replicate &&
-    git pull &&
-    bundle install &&
-    bundle exec whenever --update-crontab &&
+    cd /home/jacob/code/replicate
+    git pull
+    (bundle check || bundle install)
+    bundle exec whenever --update-crontab
     pkill -TERM -f sidekiq || true
-    rm -f tmp/pids/sidekiq.pid
-    mkdir -p log tmp/pids
-    nohup /home/jacob/bin/envwrap bash -lc 'cd /home/jacob/code/replicate && exec bundle exec sidekiq -e production -c 15 -L log/sidekiq.log --pidfile tmp/pids/sidekiq.pid' >> log/sidekiq.out 2>&1 &
+    nohup exec bundle exec sidekiq -c 15 >> sidekiq.log 2>&1 &
   CMD
 end
 
 every 1.minutes do
-  sh "cd /home/jacob/code/replicate && bin/rails runner -e production 'CronTestWorker.perform_async' >> /home/jacob/cron_log.log 2>&1"
+  sh "cd /home/jacob/code/replicate && bin/rails runner 'CronTestWorker.perform_async' >> /home/jacob/cron_log.log 2>&1"
 end
