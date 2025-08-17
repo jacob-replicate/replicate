8c12fbd9a36900df1ea036c21c1944ac59b04553
Jacob Comer <jacob@jacobcomer.com>
2025-08-16 22:23:35 -0400

    Generic commit message

--- app/workers/cron_test_worker.rb
+++
@@ -1,9 +0,0 @@
-class CronTestWorker
-  include Sidekiq::Worker
-
-  def perform(path = "/home/jacob/cron_test_v7.txt")
-    File.open(path, "a") do |file|
-      file.puts "#{Time.now} - #{Contact.count}"
-    end
-  end
-end
