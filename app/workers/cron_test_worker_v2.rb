b6870701452e4ac81146df9bfdae59dccfc77eae
Jacob Comer <jacob@jacobcomer.com>
2025-08-18 19:41:11 -0400

    Generic commit message

--- app/workers/cron_test_worker_v2.rb
+++ app/workers/cron_test_worker_v2.rb
@@ -1,7 +1,7 @@
 class CronTestWorkerV2
   include Sidekiq::Worker
 
-  def perform(path = "/home/jacob/cron_new_env.txt")
+  def perform(path = "/home/jacob/cron_new_env_v2.txt")
     File.open(path, "a") do |file|
       file.puts "#{Time.now} - #{Contact.count} - #{ENV["BOUNCER_API_KEY"]}"
     end
