8a8c44e947c26facb204eb8eda425c25eb89f0f0
Jacob Comer <jacob@jacobcomer.com>
2025-10-05 09:07:58 -0400

    Generic commit message

--- app/services/sanitize_ai_content.rb
+++ app/services/sanitize_ai_content.rb
@@ -27,9 +27,8 @@ class SanitizeAiContent
 
     response.gsub!("\n", "")
     response.gsub!(HINT_LINK, "")
-    response.gsub!('<b>', bold_start_placeholder)
-    response.gsub!(bold_start_replacement, bold_start_placeholder)
-    response.gsub!("</b>", bold_end_placeholder)
+    response.gsub!('<b>', "")
+    response.gsub!("</b>", "")
     response.gsub!("<html>", "")
     response = strip_tags(response).squish
     response.gsub!("Hey Alex,", "")
