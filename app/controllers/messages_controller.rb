d8e595b04d71d927b39b06254472b7703a21f1aa
Jacob Comer <jacob@jacobcomer.com>
2025-10-11 11:26:56 -0400

    Generic commit message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,8 @@ class MessagesController < ApplicationController
     conversation = Conversation.where(channel: :web).find_by(id: params[:conversation_id])
     return head :not_found unless conversation.present?
 
+    return head :ok if params[:content].to_s.include?("isTrusted")
+
     message = conversation.messages.create!(content: params[:content], user_generated: true)
     head :ok
   end
