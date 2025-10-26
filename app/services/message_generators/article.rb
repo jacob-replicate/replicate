e25a4f75f8f10c6a80cfb678a5404aa0f27797e6
Jacob Comer <jacob@jacobcomer.com>
2025-10-26 15:50:12 -0400

    Generic commit message

--- app/services/message_generators/article.rb
+++ app/services/message_generators/article.rb
@@ -2,10 +2,12 @@ module MessageGenerators
   class Article < MessageGenerators::Base
     def deliver_intro
       if @conversation.web?
-        deliver_elements([AvatarService.jacob_avatar_row], false, true)
+        broadcast_to_web(type: "element", message: AvatarService.coach_avatar_row, user_generated: false)
         broadcast_to_web(type: "loading", user_generated: false)
-        broadcast_to_web(type: "element", message: Prompts::ArticleIntro.new(conversation: @conversation).call, user_generated: false)
+        reply = Prompts::ArticleIntro.new(conversation: @conversation).call
+        broadcast_to_web(type: "element", message: reply, user_generated: false)
         broadcast_to_web(type: "done")
+        @conversation.messages.create!(content: "<p>#{AvatarService.coach_avatar_row}</p>#{reply}", user_generated: false)
       elsif @conversation.email?
         return
       end
@@ -13,10 +15,12 @@ module MessageGenerators
 
     def deliver_reply
       if @conversation.web?
-        deliver_elements([AvatarService.jacob_avatar_row], false, true)
+        broadcast_to_web(type: "element", message: AvatarService.coach_avatar_row, user_generated: false)
         broadcast_to_web(type: "loading", user_generated: false)
-        broadcast_to_web(type: "element", message: Prompts::ArticleReply.new(conversation: @conversation).call, user_generated: false)
+        reply = Prompts::ArticleReply.new(conversation: @conversation).call
+        broadcast_to_web(type: "element", message: reply, user_generated: false)
         broadcast_to_web(type: "done")
+        @conversation.messages.create!(content: "<p>#{AvatarService.coach_avatar_row}</p>#{reply}", user_generated: false)
       elsif @conversation.email?
         return
         deliver_elements([Prompts::CoachingReply])
