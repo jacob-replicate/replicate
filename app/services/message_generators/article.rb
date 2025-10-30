b7e2c4be1b8ae3538ab18c6b31643a4c15d38f84
Jacob Comer <jacob@jacobcomer.com>
2025-10-29 22:55:25 -0400

    Generic commit message

--- app/services/message_generators/article.rb
+++ app/services/message_generators/article.rb
@@ -2,12 +2,11 @@ module MessageGenerators
   class Article < MessageGenerators::Base
     def deliver_intro
       if @conversation.web?
-        broadcast_to_web(type: "element", message: AvatarService.coach_avatar_row, user_generated: false)
         broadcast_to_web(type: "loading", user_generated: false)
         reply = Prompts::ArticleIntro.new(conversation: @conversation).call
         broadcast_to_web(type: "element", message: reply, user_generated: false)
         broadcast_to_web(type: "done")
-        @conversation.messages.create!(content: "<p>#{AvatarService.coach_avatar_row}</p>#{reply}", user_generated: false)
+        @conversation.messages.create!(content: "#{reply}", user_generated: false)
       elsif @conversation.email?
         return
       end
