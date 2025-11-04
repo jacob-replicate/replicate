eb908215e2977785f26d545f989d0d53eb3c9b96
Jacob Comer <jacob@jacobcomer.com>
2025-11-04 00:07:18 -0500

    Generic commit message

--- app/services/message_generators/coaching.rb
+++ app/services/message_generators/coaching.rb
@@ -45,7 +45,7 @@ module MessageGenerators
         global_message_count = global_messages.count
 
         if turn == 3 && suggested_messages.count == 2
-          broadcast_to_web(type: "element", message: "#{AvatarService.jacob_avatar_row}<p>Don't try to win. <a href='https://gist.github.com/jacob-comer/9bba483ddd9ee3f3c379246bcba17873' class='text-blue-700 font-semibold hover:underline underline-offset-2' target='_blank'>The prompt</a> is a loop. It keeps asking hard SRE questions until you don't have a great reply.</p><p>Try answering this next one without multiple choice. How would your <span class='font-semibold'>ideal system</span> handle the pressure?</p><p class='mb-6'>GPT pokes holes in your best ideas, and you learn something new about infra. Improv the details as you go.</p>", user_generated: false)
+          broadcast_to_web(type: "element", message: "#{AvatarService.jacob_avatar_row}<p>Don't try to win. <a href='https://gist.github.com/jacob-comer/9bba483ddd9ee3f3c379246bcba17873' class='text-blue-700 font-semibold hover:underline underline-offset-2' target='_blank'>The prompt</a> is a loop. It keeps asking hard SRE questions until you don't have a great reply.</p><p>Try answering this next one without multiple choice. How would your <span class='font-semibold'>ideal system</span> handle the pressure?</p><p class='mb-6'>Improv the details, and let GPT poke holes in your best ideas. You learn the most about infra during a SEV-1.</p>", user_generated: false)
         end
 
         if engaged_messages.count == 5
