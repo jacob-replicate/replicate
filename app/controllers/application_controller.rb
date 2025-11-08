d3bcc1c45bdb809f415596fa866e9a0792ba4bff
Jacob Comer <jacob@jacobcomer.com>
2025-11-07 19:50:57 -0500

    Generic commit message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :skip_blocked_ips
   skip_before_action :verify_authenticity_token
 
   private
+
+  def skip_blocked_ips
+    return head(:not_found) if request.remote_ip == '35.146.19.108'
+  end
 
   def verify_admin
     raise "Not found" unless (request.remote_ip == "98.249.45.68" || Rails.env.development?)
