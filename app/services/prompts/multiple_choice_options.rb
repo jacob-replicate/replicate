b591a111fc1184c46026531a8509421788279d18
Jacob Comer <jacob@jacobcomer.com>
2025-11-08 10:22:44 -0500

    Generic commit message

--- app/services/prompts/multiple_choice_options.rb
+++ app/services/prompts/multiple_choice_options.rb
@@ -2,13 +2,14 @@ module Prompts
   class MultipleChoiceOptions < Prompts::Base
     def call
       parallel_batch_process(format: false) do |elements|
-        [2,3].include?(elements.size) && elements.all? { |opt| SanitizeAiContent.call(opt).length <= 110 && opt.exclude?("*") }
+        Rails.logger.info("MultipleChoiceOptions - elements: #{elements.inspect}")
+        [2,3].include?(elements.size) && elements.all? { |opt| SanitizeAiContent.call(opt).length <= 100 && opt.exclude?("*") }
       end
     end
 
     def fetch_elements
       options = (JSON.parse(fetch_raw_output)["options"] || JSON.parse(fetch_raw_output)[:options]) rescue []
-      options.shuffle
+      options.shuffle.map { |x| x.gsub("*", "") }
     end
   end
 end
