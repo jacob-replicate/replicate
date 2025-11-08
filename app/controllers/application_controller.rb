108656cd57f06ca2c3741df3362120dad43cf106
Jacob Comer <jacob@jacobcomer.com>
2025-11-08 10:08:56 -0500

    Generic commit message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,8 +32,8 @@ class ApplicationController < ActionController::Base
     # quick heuristics: block only when UA looks like a CLI and the request is not asking for HTML
     if (ua.blank? || ua.match?(non_browser) || ua.length < 10) && !accept.include?('text/html')
       Rails.logger.info "Non-browser request from #{request.remote_ip} ua=#{ua.inspect} accept=#{accept.inspect}"
-      # ban_current_ip
-      # return head(:ok)
+      ban_current_ip
+      return head(:ok)
     end
 
     nil
